3d90acc8457a5dad125b97e0df04cef5f3accd15
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-20 13:22:34 -0700

    Split one test into two

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -25,15 +25,21 @@ class HomePageTest(TestCase):
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
-        response = home_page(request)
+        _ = home_page(request)
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
