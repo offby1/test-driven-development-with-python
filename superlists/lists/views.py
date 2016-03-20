77b29044b1c31cf690034548a3ef1cd609a4d679
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-20 12:00:39 -0700

    Feecks test.
    I don't like saying "redirect ('/')"; that mixes up URL dispatch with
    view logic.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
         item = Item.objects.create(text=request.POST['item_text'])
-        item.save()
+        return redirect('/')
 
     return render(request, 'home.html', {'items': Item.objects.all()})
