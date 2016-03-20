eacb7e12e4d050a22263871c74476ea459f3dc7a
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-20 10:56:54 -0700

    Save input on POST

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        item = Item.objects.create(text=request.POST['item_text'])
+        item.save()
+
     return render(request, 'home.html')
