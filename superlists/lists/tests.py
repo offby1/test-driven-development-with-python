cb5d1b589edadaebf14d5aa6af063166120a93b3
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-20 10:36:25 -0700

    New failing test: POST

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -17,3 +17,12 @@ class HomePageTest(TestCase):
         actual_content = response.content.decode()
         expected_content = render_to_string('home.html')
         self.assertEqual(actual_content, expected_content)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
