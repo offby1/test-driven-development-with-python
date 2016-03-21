46d9449b3eb86a002360cf73062f746547348b73
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-20 17:00:02 -0700

    Let /lists/ show the home page

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls import include, url
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/([0-9]+)/$', views.view_list, name='view_list'),
-    url(r'^lists/([0-9]+)/add_item$', views.add_item, name='add_item'),
+    url(r'^$'                        , views.home_page),
+    url(r'^lists/$'                  , views.home_page),
+    url(r'^lists/new$'               , views.new_list,  name='new_list' ),
+    url(r'^lists/([0-9]+)/$'         , views.view_list, name='view_list'),
+    url(r'^lists/([0-9]+)/add_item$' , views.add_item,  name='add_item' ),
 ]
