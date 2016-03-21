fc2a04f7629fafc36234caf7efbfb0720cf305cd
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-20 17:35:13 -0700

    Mollify flake8

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -28,7 +28,7 @@ class NewListTest(TestCase):
 class NewItemTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
+        List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
@@ -41,9 +41,8 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-
     def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
+        List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
@@ -101,7 +100,6 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.get('/lists/{}/'.format(list_.id))
         self.assertTemplateUsed(response, 'list.html')
-
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
