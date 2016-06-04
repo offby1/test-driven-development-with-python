245816c6bb234cb3d769c26d998f9ad48cea5344
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-06-04 11:12:53 -0700

    use LiveServerTestCase

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,12 +1,12 @@
 import sys
 
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.staticfiles.testing import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -31,7 +31,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
@@ -60,7 +60,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -103,7 +103,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
