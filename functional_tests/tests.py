4b60cd53dfd4d117e6bb621183f8735a71d5e3b2
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-06-04 18:43:53 -0700

    Add test_cannot_add_empty_list_items ...
    ... and tearDown method

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -24,6 +24,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
+    def tearDown(self):
+        self.browser.quit()
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -126,3 +129,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=5
         )
 
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
